eeee1fbd71bd03f6879444b3448dec183668303a
Nick12344321 <daynick510@gmail.com>
2025-09-25 22:54:30 +0530

    Done

---
+++ Stoney_codes/minTime.py
@@ -0,0 +1,10 @@
+def minTimeToVisitAllPoints(points):
+    res=0
+    x1,y1=points.pop()
+    while points:
+        x2,y2=points.pop()
+        res+=max(abs(x2-x1),abs(y2-y1))
+        x1,y1=x2,y2
+    return res
+points=[[1,1],[3,4],[-1,0]]
+print(minTimeToVisitAllPoints(points))
