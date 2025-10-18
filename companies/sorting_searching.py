1773db44494d3d9a4c6eecd8461747817be23f7b
Nish2005karsh <nishkarshpandey059@gmail.com>
2025-10-18 18:48:54 +0530

    Done

---
+++ companies/sorting_searching.py
@@ -0,0 +1,14 @@
+# Implement binary search 
+# def binarySearch(arr,low,high,target):
+#     while low<high:
+#         mid=low+(high-low)//2
+#         if(arr[mid]==target):
+#             return mid
+#         elif(arr[mid]>target):
+#             high=mid-1
+#         else:
+#             low=mid+1
+#     return -1
+# arr=[1,2,3,4,5,6,7,8,9,10]
+# target=5
+# print(binarySearch(arr,0,len(arr)-1,target))
