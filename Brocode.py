53a01fa92fd10737bf3f9c4ffdba67b81d59196f
Nish2005karsh <nishkarshpandey059@gmail.com>
2025-10-23 22:37:42 +0530

    Practice

--- Brocode.py
+++ Brocode.py
@@ -50,6 +50,22 @@
 #             left+=1
 
 # Valid paranthesis
+# def isValid(s):
+#     stack=[]
+#     for char in s:
+#         if char == '(':
+#             stack.append(')')
+#         elif char=='{':
+#             stack.append('}')
+#         elif char== "[":
+#             stack.append(']')
+#         else:
+#             if not stack or stack.pop()!=char:
+#                 return False
+#     return len(stack)==0
+
+
+
 
 
 
