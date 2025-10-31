742b4b76ae54d80c9b38f4d534e37d2fe5901605
Nish2005karsh <nishkarshpandey059@gmail.com>
2025-10-31 09:43:38 +0530

    Practice

--- Brocode.py
+++ Brocode.py
@@ -273,7 +273,16 @@
 #         return res
 # graph numericals
 # Inorder traversal
-
+class Solution:
+    def inorderTraversal(self,root):
+        res=[]
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            res.append(node.val)
+            inorder(node.right)
+            
 
 
 
